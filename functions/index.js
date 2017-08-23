2fef8af97189984ebca41fb498b6b9be70b45757
tungbt94 <tungbt94@gmail.com>
2017-08-24 02:43:14 +0700

    render facts

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,34 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
 const express = require('express');
 const engines = require('consolidate');
+
+const firebaseApp = firebase.initializeApp(
+  functions.config().firebase
+);
+
+function getFacts() {
+  const ref = firebaseApp.database().ref('facts');
+  return ref.once('value').then(snap => snap.val());
+}
 
 const app = express();
 app.engine('hbs', engines.handlebars);
 app.set('views', './views');
 app.set('view engine', 'hbs')
 
-app.get('/timestamp', (request, response) => {
-  response.send(`${Date.now()}`);
+app.get('/', (request, response) => {
+  response.set('Cache-Control', 'public, max-age=300, s-max-age=600');
+  getFacts().then(facts => {
+    response.render('index', { facts });
+  });
 });
 
-app.get('/timestamp-cached', (request, response) => {
+app.get('/facts.json', (request, response) => {
   response.set('Cache-Control', 'public, max-age=300, s-max-age=600');
-  response.send(`${Date.now()}`);
+  getFacts().then(facts => {
+    response.json(facts);
+  });
 });
 
 exports.app = functions.https.onRequest(app);
